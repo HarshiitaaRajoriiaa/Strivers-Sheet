301aed0a0ea808e2deeeea4cc351e5bcfd869fac
Harshita Rajoria <harshitarajoria02@gmail.com>
2024-06-25 14:13:32 +0530

    commit

--- Strivers A2Z DSA Sheet/Step01_Basics/Lec01JavaBasics/NFibbonaciNumber.java
+++ Strivers A2Z DSA Sheet/Step01_Basics/Lec01JavaBasics/NFibbonaciNumber.java
@@ -1,24 +1,25 @@
 package Lec01JavaBasics;
 
+import java.util.Scanner;
+
 public class NFibbonaciNumber {
     public static void main(String[] args) {
-        System.out.println(fibonacci(8));
+		Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
         
-    }
-    public static int fibonacci(int n){
-        int left = 1;
-		int LoLeft = 1;
-		int output = -1;
-		if(n==1 || n==2){
-			output =1;
-		}
+        if (n == 1 || n == 2) {
+            System.out.println(1);
+            return;
+        }
+		int f1 = 1; 
+        int f2 = 1; 
+        int curr = 0;
 		for(int i=3;i<=n;i++){
-			output = left + LoLeft;
-			LoLeft=left;
-			left = output;
-			
+			curr = f1 + f2; 
+            f1 = f2; 
+            f2 = curr;
 		}
-		
-		return output;
-    }
+		System.out.println(curr);
+	
+	}
 }
