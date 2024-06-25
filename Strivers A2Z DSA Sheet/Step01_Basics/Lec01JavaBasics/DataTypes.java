afee0fac646cd3089761d15c0da152d46d6fbddd
Harshita Rajoria <harshitarajoria02@gmail.com>
2024-06-25 10:35:16 +0530

    commit

---
+++ Strivers A2Z DSA Sheet/Step01_Basics/Lec01JavaBasics/DataTypes.java
@@ -0,0 +1,26 @@
+package Lec01JavaBasics;
+public class DataTypes{
+    public static void main(String[] args) {
+        System.out.println(dataTypeSize("Integer"));
+        
+    }
+    public static int dataTypeSize(String str) {
+        int output = 0;
+        if (str.equals("Character")) {
+            output = 2; 
+        }
+        if (str.equals("Integer")) {
+            output = 4; 
+        }
+        if (str.equals("Long")) {
+            output = 8; 
+        }
+        if (str.equals("Float")) {
+            output = 4; 
+        }
+        if (str.equals("Double")) {
+            output = 8; 
+        }
+        return output;
+    }
+}
