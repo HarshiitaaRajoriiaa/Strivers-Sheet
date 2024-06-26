a9631d51dab3e3a377f3d5a623a45cf04fd2f8df
Harshita Rajoria <harshitarajoria02@gmail.com>
2024-06-26 15:52:38 +0530

    commit

--- Strivers A2Z DSA Sheet/Step01_Basics/Lec02LogicalThinking/Patterns.java
+++ Strivers A2Z DSA Sheet/Step01_Basics/Lec02LogicalThinking/Patterns.java
@@ -7,7 +7,11 @@ class Patterns {
             // nTriangle1(10);
             // nTriangle2(5);
             // seeding(5);
-            nNumberTriangle(5);
+            // nNumberTriangle(5);
+            // nBinaryTriangle(7);
+            // nNumberTriangle2(8);
+            // nLetterTriangle(5);
+            nLetterTriangle1(4);
         }
         public static void nForest1(int n) {
             // Write your code here
@@ -56,13 +60,53 @@ class Patterns {
         }
         public static void nNumberTriangle(int n) {
             // Write your code here
-            for(int i = 1;i<=n;i++){
-                for(int j=1;j<=n;j++){
+            for(int i=1;i<=n;i++){
+                for(int j = 1 ; j<=n-i+1;j++){
                     System.out.print(j);
                 }
                 System.out.println(" ");
-                }
+            }
+            
         }
+        public static void nBinaryTriangle(int n) {
+            for( int i = 1 ; i <=n;i++){
+                for(int j = 1 ; j <=i ; j++){
+                    if((i+j)%2 == 0){
+                        System.out.print(1+ " ");
+                    }
+                    else{
+                        System.out.print(0+ " ");
+                    }
+                }
+                System.out.println("");
+            }
+        }
+        public static void nNumberTriangle2(int n) {
+            int counter = 1;
+            for(int i = 1 ; i <=n ; i++){
+                for ( int j = 1 ; j <=i ; j++){
+                    System.out.print(counter + " ");
+                    counter++;
+                }
+                System.out.println("");
+            }
+        }
+        public static void nLetterTriangle(int n) {
+            for( int  i = 1 ; i <=n ; i ++){
+                for(char j = 'A' ; j <='A'+ i-1 ; j++){
+                    System.out.print( j + " ");
+                }
+                System.out.println("");
+            }
+        }
+        public static void nLetterTriangle1(int n) {
+            for( int  i = 1 ; i <=n ; i ++){
+                for(char j = 'A' ; j <='A'- i+1 ; j++){
+                    System.out.print( j + " ");
+                }
+                System.out.println("");
+            }
+         }
 }
 
     
