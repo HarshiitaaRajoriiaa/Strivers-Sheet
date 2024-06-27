d3cad6c67bdbc56b25b2fcb60b7d5d0a3cd083e4
Harshita Rajoria <harshitarajoria02@gmail.com>
2024-06-27 20:57:31 +0530

    commit

---
+++ Strivers A2Z DSA Sheet/Step01_Basics/Lec03JavaCollectionFramework/CountDigit.java
@@ -0,0 +1,19 @@
+package Step01_Basics.Lec03JavaCollectionFramework;
+
+public class CountDigit {
+    public static void main(String[] args) {
+        System.out.println(countDigit(123));
+    }
+    public static int countDigit(int N){
+        String digit = String.valueOf(N);
+        int counter = 0;
+        for(int i = 0 ; i<digit.length() ; i++){
+            int current = Character.getNumericValue(digit.charAt(i));
+            if(current != 0 && N % current == 0  ){
+                counter++;
+            }
+        }
+        return counter;
+
+    }
+}
