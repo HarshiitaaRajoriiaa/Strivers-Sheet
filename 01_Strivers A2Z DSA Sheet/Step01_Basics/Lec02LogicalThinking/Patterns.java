b9dc9b370b534b4e26ffeb09891d033ed1c53981
Harshita Rajoria <harshitarajoria02@gmail.com>
2024-07-01 15:41:39 +0530

    commit

--- 01_Strivers A2Z DSA Sheet/Step01_Basics/Lec02LogicalThinking/Patterns.java
+++ 01_Strivers A2Z DSA Sheet/Step01_Basics/Lec02LogicalThinking/Patterns.java
@@ -7,7 +7,12 @@ class Patterns {
             // nTriangle1(10);
             // nTriangle2(5);
             // seeding(5);
-            nNumberTriangle(5);
+            // nNumberTriangle(5);
+            // starPyramid(10);
+            // starPyramid2(4);
+            // starPyramid3(5);
+            // nStarTriangle(5);
+            numberCrown(5);
             // nBinaryTriangle(7);
             // nNumberTriangle2(8);
             // nLetterTriangle(5);
@@ -75,12 +80,53 @@ class Patterns {
             
         }
         //07
-        for(int  i = 0 ; i<n;i++){
-            for(int j = 0 ; j<2*n ; j++){
-                
+
+        public static void starPyramid(int n){
+            for(int  i = 0 ; i<n;i++){
+                    //Spaces:
+                    for(int j = 0 ; j<n-1-i;j++){
+                        System.out.print(" ");
+                    }
+                    //Stars:
+                    for(int j = 0 ; j<2*i+1;j++){
+                        System.out.print("*");
+                    }
+                    //Spaces:
+                    for(int j = 0 ; j<n-1-i;j++){
+                        System.out.print(" ");
+                    }
+                System.out.println(" ");
             }
         }
-
+        //08
+        public static void starPyramid2(int n){
+            for(int  i = 0 ; i<n;i++){
+                    //Spaces:
+                    for(int j = 0 ; j<i;j++){
+                        System.out.print(" ");
+                    }
+                    //Stars:
+                    for(int j = 0 ; j<2*n-(1+2*i);j++){
+                        System.out.print("*");
+                    }
+                    //Spaces:
+                    // for(int j = 0 ; j<i;j++){
+                    //     System.out.print(" ");
+                    // }
+                System.out.println();
+            }
+        }
+        //09
+        public static void starPyramid3(int n){
+            starPyramid(n);
+            starPyramid2(n);
+        }
+        //10
+        public static void nStarTriangle(int n) {
+            // Write your code here
+            nForest2(n);
+            seeding(n-1);
+        } 
         //11
         public static void nBinaryTriangle(int n) {
             for( int i = 1 ; i <=n;i++){
@@ -93,6 +139,24 @@ class Patterns {
                     }
                 }
                 System.out.println("");
+            }
+        }
+        //12
+        public static void numberCrown(int n){
+            for(int  i = 1 ; i<=n;i++){
+                //numbers:
+                for(int j = 1 ; j<=i;j++){
+                    System.out.print(j);
+                }
+                //Spaces:
+                for(int j = 0 ; j<2*n-(1+2*i);j++){
+                    System.out.print("*");
+                }
+                //Spaces:
+                // for(int j = 0 ; j<i;j++){
+                //     System.out.print(" ");
+                // }ol
+            System.out.println();
             }
         }
         //13
