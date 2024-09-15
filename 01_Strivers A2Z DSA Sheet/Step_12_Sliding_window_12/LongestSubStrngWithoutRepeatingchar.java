ee12a5e4d9077a7fcf63c7280a1794a9646d3085
HarshitaRajoria <harshitarajoria@gmail.com>
2024-09-15 16:16:16 +0530

    Sliding Window

---
+++ 01_Strivers A2Z DSA Sheet/Step_12_Sliding_window_12/LongestSubStrngWithoutRepeatingchar.java
@@ -0,0 +1,9 @@
+//?https://leetcode.com/problems/longest-substring-without-repeating-characters/
+public class LongestSubStrngWithoutRepeatingchar {
+    public static void main(String[] args) {
+    }
+    // * Brute Force Method: generate all the substrings with for loops and count how many substrings are satisfying the condition of non-repeating character.
+    
+
+
+}
