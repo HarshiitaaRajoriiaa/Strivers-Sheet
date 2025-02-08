5050c8a6c7f5a006722c761d15db314f6dbddd68
HarshiitaaRajoriiaa <harshitarajoria02@gmail.com>
2025-02-08 17:19:39 +0530

    stack basic done commit it

--- 01_Strivers A2Z DSA Sheet/Step09_StacksQueue_30/Learning/BalancedPara.java
+++ 01_Strivers A2Z DSA Sheet/Step09_StacksQueue_30/Learning/BalancedPara.java
@@ -1,5 +1,26 @@
 package Step09_StacksQueue_30.Learning;
-//
+import java.util.Stack;
+//?https://leetcode.com/problems/valid-parentheses/
 public class BalancedPara {
-    
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<Character>();
+        //stack should be empty at end of the loop,
+        // push opening ones, and compare the closing ones when encountered with last element
+        // s.length() %2 != 0 return false
+        int l = s.length();
+        if(l%2 != 0 ) return false;
+        for(int i =0 ; i <l; i++){
+            char it = s.charAt(i);
+            if(it == '(' || it == '[' || it == '{') st.push(it);
+            else{
+                if(st.isEmpty()) return false;
+                char ch = st.pop();
+                //compare ( == ) , { == } , [ == ]
+                // char it = s.charAt(i);
+                if((it == ')' && ch == '(') ||  (it == ']' && ch == '[') || (it == '}' && ch == '{')) continue;
+                else  return false;    
+            }
+        }
+        return st.isEmpty();
+    }
 }
---
+++ 01_Strivers A2Z DSA Sheet/Step09_StacksQueue_30/Learning/Conversions.java
@@ -0,0 +1,10 @@
+package Step09_StacksQueue_30.Learning;
+ // todo Prefix , Infix , Postfix conversions
+ // operators , operands , priorities
+//  Infix -> Postfix , Infix -> Prefix 
+//  Prefix -> Infix , Prefix -> Postfix
+//  Postfix -> Infix , Postfix -> Prefix
+// precedence  ^ > ( * , / ) > ( + , - )
+public class Conversions {
+    
+}
