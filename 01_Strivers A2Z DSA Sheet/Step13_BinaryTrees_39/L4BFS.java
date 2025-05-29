65cbdfc24b4ce620ca61104ee11048bef24e666d
HarshiitaaRajoriiaa <harshitarajoria02@gmail.com>
2025-05-29 13:27:40 +0530

    BFS done

---
+++ 01_Strivers A2Z DSA Sheet/Step13_BinaryTrees_39/L4BFS.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class L4BFS {
+    public static void main(String[] args) {
+
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        if (root == null) return ans;
+        q.offer(root);
+
+        while (!q.isEmpty()) {
+            int level = q.size();
+            ArrayList<Integer> sList = new ArrayList<>();
+            for (int i = 0; i < level; i++) {
+                if (q.peek().left != null) q.offer(q.peek().left);
+                if (q.peek().right != null) q.offer(q.peek().right);
+                sList.add(q.poll().val);
+            }
+        ans.add(sList);
+
+        }
+        return ans;
+    }
+}
